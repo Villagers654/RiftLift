3bee5058e664197b0048779cf7541fb7eed6c39d
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-08-09 23:41:48 -0400

    Pin the Lone Echo compatible payload

--- src/riftlift/runtime.py
+++ src/riftlift/runtime.py
@@ -16,9 +16,9 @@ from .util import RiftLiftError, download, linux_to_windows, run
 PROTON_VERSION = "GE-Proton11-3"
 PROTON_URL = f"https://github.com/GloriousEggroll/proton-ge-custom/releases/download/{PROTON_VERSION}/{PROTON_VERSION}.tar.gz"
 PROTON_SHA256 = "861c2edc8d40d051fb1e7a692deb953be52bd339c46d90f2b7dde50ddad91266"
-REVIVE_VERSION = "riftlift-0.4.1"
-REVIVE_URL = "https://github.com/Villagers654/RiftLift/releases/download/v0.4.1/riftlift-revive.zip"
-REVIVE_SHA256 = "b829bc5004fda5cc236982ed3b3d523552ab75de03789431195e26a730976be8"
+REVIVE_VERSION = "riftlift-0.5.1"
+REVIVE_URL = "https://github.com/Villagers654/RiftLift/releases/download/v0.5.1/riftlift-compat.zip"
+REVIVE_SHA256 = "6a397eefa0fa17688a23831e6d4a4c44ad40e888259498bcc8f90a47f1c457e7"
 
 
 @dataclass(frozen=True, slots=True)
